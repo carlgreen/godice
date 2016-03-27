f2f864bf7673c3c11af89aeea169a721dcbd238d
Carl Green <carlgreen@gmail.com>
2016-03-27 18:25:34 +1300

    make number of words configurable on command line

--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"crypto/rand"
+	"flag"
 	"fmt"
 	"math/big"
 	"strings"
 )
 
-var numWords int = 6
-
 func main() {
+	numWordsPtr := flag.Int("words", 6, "number of words to generate")
+	flag.Parse()
+
 	var words []string
 	wordCnt := big.NewInt(int64(len(Words)))
-	for i := 0; i < numWords; i++ {
+	for i := 0; i < *numWordsPtr; i++ {
 		r, _ := rand.Int(rand.Reader, wordCnt)
 		words = append(words, Words[int(r.Int64())])
 	}
